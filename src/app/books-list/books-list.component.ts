d7a3478f04b9c367b3153fc7bb2888bf90d300c4
sergioyanez <sergiomyanez02@gmail.com>
2020-11-18 00:51:07 -0300

    se agregò interface book ,template del componente book-list

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './Book';
 
 @Component({
   selector: 'app-books-list',
@@ -6,15 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-list.component.scss']
 })
 export class BooksListComponent implements OnInit {
-  book = {
-    "name":"Libro 1",
-    "autor" : "Autor 1",
-    "genere" : "Género 1",
-    "price": 1000,
-    "stock" : 10,
-    "image" : "assets/img/libro1.jpg"
-
-  }
+  books: Book[] = [
+ 
+    {
+      name:"Libro 1",
+      autor : "Autor 1",
+      genere : "Género 1",
+      price: 1000,
+      stock : 10,
+      image : "assets/img/libro1.jpg",
+      clearance: false,
+    },
+    {
+      name:"Libro 2",
+      autor : "Autor 2",
+      genere : "Género 2",
+      price: 2000,
+      stock : 0,
+      image : "assets/img/libro2.jpg",
+      clearance: false,
+    },
+    {
+      name:"Libro 3",
+      autor : "Autor 3",
+      genere : "Género 3",
+      price: 500,
+      stock : 800,
+      image : "assets/img/libro3.jpg",
+      clearance: true,
+    }
+]
   constructor() { }
 
   ngOnInit(): void {
